846adac5be12dd8f80a711900059ffd445f1d6ca
Andrew Kats <akats98@uw.edu>
2019-06-04 17:06:03 -0700

    First plot

--- server.R
+++ server.R
@@ -1,8 +1,24 @@
 library(shiny)
+library(plotly)
 library(dplyr)
+library(ggplot2)
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
+deaths <- read.csv("./data/NCHS_Leading_Causes_of_Death_United_States.csv")
+
+shinyServer(function(input, output) {
+  attach(deaths)
+  sort_years <- deaths[order(-Year),]
+  detach(deaths)
   
+  # Creates chart for age adjusted death rates for disease in a given year
+  output$chart <- renderPlotly({
+    data <- data.frame(
+      select(sort_years, Age.adjusted.Death.Rate, Year, Cause.Name, State) %>% filter(Year == input$year) %>% filter(Cause.Name == input$cause) %>% transmute(Age.adjusted.Death.Rate, State), stringsAsFactors = FALSE)
+    plot_ly(data, x = data$State, y = data$Age.adjusted.Death.Rate, type = 'bar', text = data$Age.adjusted.Death.Rate, textposition = 'auto', marker = list(color = "#305f72")) %>% 
+      layout(title = paste0("Age Adjusted Death Rates per State"), xaxis = list(title = "States"), yaxis = list(title = "Age Adjusted Death Rates"))
+  })
 })
+ 
